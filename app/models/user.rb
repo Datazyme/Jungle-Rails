ff0c6814af9f8991564cf72b452de6c3ac504a4b
Datazyme <anakuzmin2@gmail.com>
2023-04-29 23:34:24 +0000

    tests check user authentication still case matters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness:{ case_sensitive:false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length:{ minimum:5 }
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    @email_strip = email.strip 
+    user = User.find_by_email(@email_strip)
       if user && user.authenticate(password)
         return user
       end
--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -4,88 +4,85 @@ RSpec.describe User, type: :model do
   
   describe 'Validations' do 
     it "allows a user to register if all fields filled out" do 
-      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password =>"testing", :password_confirmation => "testing")
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
       expect(@user).to be_valid 
     end 
 
     it "checks if first_name is blank" do 
-      @user = User.new(:first_name => nil, :last_name => "Testorius", :email => "test@testorius.com", :password =>"testing", :password_confirmation => "testing")
+      @user = User.new(:first_name => nil, :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
       expect(@user).not_to be_valid 
       expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
     end 
 
     it "checks if last_name is blank" do 
-      @user = User.new(:first_name => "TEST", :last_name => nil, :email => "test@testorius.com", :password =>"testing", :password_confirmation => "testing")
+      @user = User.new(:first_name => "TEST", :last_name => nil, :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
       expect(@user).not_to be_valid 
       expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
     end 
 
     it "checks if email is blank" do 
-      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => nil, :password =>"testing", :password_confirmation => "testing")
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => nil, :password => "testing", :password_confirmation => "testing")
       expect(@user).not_to be_valid 
       expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
     end 
 
     it "checks if passwords don't match and doesn't create user if not" do
-      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password =>"testing", :password_confirmation => "testorius")
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testorius")
       expect(@user).not_to be_valid 
       expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
     end
 
     it "password should have minimum 5 charaters" do 
-      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password =>"test", :password_confirmation => "test")
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "test", :password_confirmation => "test")
       expect(@user).not_to be_valid 
       expect(@user.errors.full_messages[0]).to eq("Password is too short (minimum is 5 characters)")
     end
 
-
-    it "emails should be unique case sensitive " do 
-      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+    it "checks if email is unique even if only case doesn't match" do 
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius",  :email => "test@testorius.com" , :password => "testing", :password_confirmation => "testing")
       @user.save
-      @user2 =User.new(:first_name => "2test", :last_name => "test",  :email => "test@test.COM" , :password => "2test1", :password_confirmation => "2test1" )
+      @user2 =User.new(:first_name => "TEST", :last_name => "Testorius",  :email => "TEST@testorius.com" , :password => "testingwing", :password_confirmation => "testingwing")
       expect(@user2).not_to be_valid 
       expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
     end
 
-    it "emails should be unique NOT case sensitive " do 
-      @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+    it "checks if email exists, NOT case sensitive" do 
+      @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
       @user.save
-      @user2 =User.new(:first_name => "2test", :last_name => "test",  :email => "test@test.com" , :password => "2test1", :password_confirmation => "2test1" )
+      @user2 = User.new(:first_name => "Testorius", :last_name => "TEST", :email => "TEST@testorius.com", :password => "testingwing", :password_confirmation => "testingwing")
       expect(@user2).not_to be_valid 
       expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
     end
   
-  describe '.authenticate_with_credentials' do 
-      it "should authenticate with credentials" do 
-        @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+  describe "authenticate_with_credentials" do 
+      it "authenticates when all credentials pass" do 
+        @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
         @user.save 
         expect(User.authenticate_with_credentials(@user.email, @user.password)).to eq(@user)
       end
 
-
-      it "should not authenticate with invalid email" do 
-        @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+      it "doesn't authenticate with wrong email" do 
+        @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
         @user.save 
-        expect(User.authenticate_with_credentials("wrong@test.com", @user.password)).to be_nil
+        expect(User.authenticate_with_credentials("wrong@testorius.com", @user.password)).to be_nil
       end
 
-      it "should not authenticate with invalid password" do 
-        @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+      it "doesn't authenticate with wrong password" do 
+        @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
         @user.save 
-        expect(User.authenticate_with_credentials(@user.email, "wrongpassword")).to be_nil
+        expect(User.authenticate_with_credentials(@user.email, "testingwing")).to be_nil
       end
 
-      it "should authenticate with extra spaces before email" do 
-        @user = User.new(:first_name => "test", :last_name => "2test",  :email => "test@test.com" , :password => "test12", :password_confirmation => "test12" )
+      it "authenticates with extra spaces before email" do 
+        @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
         @user.save 
-        expect(User.authenticate_with_credentials("   test@test.com",@user.password)).to eq(@user)
+        expect(User.authenticate_with_credentials("   test@testorius.com", @user.password)).to eq(@user)
       end
 
-
-      it "should authenticate with wrong case email" do 
-        @user = User.new(:first_name => "test", :last_name => "2test",  :email => "TeST@test.com" , :password => "test12", :password_confirmation => "test12" )
+      it "authenticates with same email different case" do 
+        @user = User.new(:first_name => "TEST", :last_name => "Testorius", :email => "test@testorius.com", :password => "testing", :password_confirmation => "testing")
         @user.save 
-        expect(User.authenticate_with_credentials("test@TeST.com",@user.password)).to eq(@user)
+        expect(User.authenticate_with_credentials("TEST@testorius.com", @user.password)).to eq(@user)
       end
     end 
   end
